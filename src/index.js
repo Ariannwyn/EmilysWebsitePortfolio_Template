9e90775dc2e5d0a3f44ef579674cb3087de1ae11
EmilyA <emilyadams.3d@gmail.com>
2020-08-26 13:02:14 -0700

    Added homemage url to package json

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.1.0";
@@ -28,7 +28,7 @@ import Index from "views/Index.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/home" render={(props) => <Index {...props} />} />
+      <Route path="/" render={(props) => <Index {...props} />} />
       <Redirect from="/" to="/home" />
     </Switch>
   </BrowserRouter>,
