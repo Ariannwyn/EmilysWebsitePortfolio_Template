19e174ab1d9665d79e2d9048108f7f43a40ff570
EmilyA <emilyadams.3d@gmail.com>
2020-11-10 16:06:13 -0800

    Changed out portfolio projects to newer ones

--- src/views/Index.js
+++ src/views/Index.js
@@ -27,8 +27,8 @@ import Download from "views/IndexSections/Download.js";
 
 //gifs
 import nasaImg from "../assets/img/NasaProject.gif";
-import portfolioImg from "../assets/img/PortfolioProject.gif";
-import devDeskImg from "../assets/img/DevDeskProject.gif";
+import bookstoreImg from "../assets/img/bookstoreProject.gif";
+import buildweekImg from "../assets/img/buildweekProject.gif";
 
 class Index extends React.Component {
   componentDidMount() {
@@ -44,6 +44,20 @@ class Index extends React.Component {
         <div className="wrapper">
           <PageHeader />
           <div className="main">
+          <Examples
+              live="https://devdeskqueuewebpt1.netlify.app/"
+              image={buildweekImg}
+              code="https://github.com/Build-Week-DevDeskQueue-1/Marketing-Page"
+              text="I was part of a team creating a neighborhood social app. This would allow people within a neighborhood to post items for sale, or missing items, etc. My role was implementing CRUD opperations to display and edit the posts, as well as ensuring the token authentication and state management on the front end."
+              skills={"React \u2022 JavaScript \u2022 Context \u2022 Cypress \u2022 Styled Components \u2022 Reactstrap"}
+            />
+          <Examples
+              live="https://the-bookstore.netlify.app/"
+              image={bookstoreImg}
+              code="https://github.com/book-store-sales/react-shopping-cart"
+              text="I was part of a team creating a bookstore site that implements React-Redux as a state management system. This application fetches data from an API to display the books listed, and allows the user to add and remove items from a shopping cart" //, modify the cart, checkout with a form, and recieve a success message on submit with their order.
+              skills={"React \u2022 JavaScript \u2022 CSS \u2022 Redux \u2022 Sass"}
+            />
             <Examples
               id="Projects"
               live="https://emily-a-nasa.netlify.app/"
@@ -52,21 +66,6 @@ class Index extends React.Component {
               text="I created a Space Photo Of The Day site that displays astronomy photos utilizing the NASA API."
               skills={"React \u2022 JavaScript \u2022 CSS \u2022 Reactstrap"}
             />
-            <Examples
-              live="https://emily-adams.netlify.app/index.html"
-              image={portfolioImg}
-              code="https://github.com/Ariannwyn/User-Interface-II/tree/emily-adamsday2"
-              text="This was the first version of my portfolio. I created a landing page, about page, and a contact form."
-              skills={"React \u2022 JavaScript \u2022 CSS \u2022 Reactstrap"}
-            />
-            <Examples
-              live="https://devdeskqueuewebpt1.netlify.app/"
-              image={devDeskImg}
-              code="https://github.com/Build-Week-DevDeskQueue-1/Marketing-Page"
-              text="I was part of a team making a ticketing system for Lambda School. My role was creating the Home and About pages."
-              skills={"HTML \u2022 CSS \u2022 LESS"}
-            />
-
             <Download id="About" />
           </div>
         </div>
